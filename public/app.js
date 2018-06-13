42801c51b15c34b5b954d4aa02bd3ee4cd5147c8
Liam Cavens <liamcavens@googlemail.com>
2018-06-13 20:28:51 +0100

    got basics working, will do more once internet it running more smooth

--- public/app.js
+++ public/app.js
@@ -1,6 +1,9 @@
 var app = function(){
     const url = "https://api.punkapi.com/v2/beers";
     makeRequest(url, requestComplete);
+
+    const select = document.querySelector('select');
+    select.addEventListener("change", handleOptionSelect);
 }
 
 const makeRequest = function (url, callbackFunction) {
@@ -24,7 +27,18 @@ const populateList = function (beers) {
         option.textContent = `${beer.name}`;
         option.value = JSON.stringify(beer);
         select.appendChild(option);
-    })
+    });
 }
+
+const handleOptionSelect = function(){
+    const beer = JSON.parse(this.value);
+    const header = document.querySelector('#beer-name');
+    const beerDesc = document.querySelector('#beer-description');
+    const beerImage = document.querySelector('#beer-image');
+
+    header.textContent = `Beer : ${beer.name}`;
+    beerDesc.textContent = `Description : ${beer.description}`;
+    beerImage.innerHTML = `<img class="beer-resize" src="${beer.image_url}" alt="${beer.name}_image">`
+};
 
 window.addEventListener('load', app);
